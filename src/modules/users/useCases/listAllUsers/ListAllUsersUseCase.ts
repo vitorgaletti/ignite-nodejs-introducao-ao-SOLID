1f48aa93ea1189c2dcea1dc62f2ffd10ed04e8ee
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-06-23 18:40:40 -0300

    :test_tube: Finished tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -10,6 +10,20 @@ class ListAllUsersUseCase {
 
   execute({ user_id }: IRequest): User[] {
     // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    const isAdminUser = this.usersRepository.findById(user_id)?.admin;
+
+    if (!isAdminUser) {
+      throw new Error("User not admin");
+    }
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
